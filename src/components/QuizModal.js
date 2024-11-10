3f8bfe74aca074024d33084e6a656f80e196f134
Jeremy Wu <jeremylwu@gmail.com>
2024-11-10 14:01:54 -0800

    Basic Quiz Modal

--- src/components/QuizModal.js
+++ src/components/QuizModal.js
@@ -1,2 +1,29 @@
+import React from 'react';
+import Popup from 'reactjs-popup';
+
+function QuizModal({ question, options, onAnswer }) {
+  const handleAnswer = (selectedOption) => {
+    onAnswer(selectedOption); // Pass the selected option back to parent component
+  };
+
+  return (
+    <Popup open={true} closeOnDocumentClick={false} modal>
+      <div>
+        <h2>Quiz Question</h2>
+        <p>{question}</p>
+        <div>
+          {options.map((option, index) => (
+            <button key={index} onClick={() => handleAnswer(option)}>
+              {option}
+            </button>
+          ))}
+        </div>
+      </div>
+    </Popup>
+  );
+}
+
+export default QuizModal;
+
 
 
