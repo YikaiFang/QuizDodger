29644432bb99d31d8a75a1637e112a0fceb46fcf
jchen278 <jonathanchen255@gmail.com>
2024-11-10 15:30:43 -0800

    testing gamescreen and losescreen

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,11 +1,21 @@
 // GameScreen.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const GameScreen = () => {
+  const navigate = useNavigate();
+
+  const goToLoseScreen = () => {
+    navigate('/LoseScreen');
+  };
+
   return (
     <div>
       <h1>Welcome to the Play Screen!</h1>
       <p>Here is where the game starts.</p>
+      <button onClick={goToLoseScreen} style={{textAlign: 'center', marginTop: 'auto'}} className="LoseButton">Lose</button>
     </div>
   );
 };
---
+++ src/screens/LoseScreen.js
@@ -0,0 +1,23 @@
+// GameScreen.js
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const GameScreen = () => {
+  const navigate = useNavigate();
+
+  const goToHomeScreen = () => {
+    navigate('/');
+  };
+
+  return (
+    <div>
+      <h1>You LOST!</h1>
+      <p>womp womp</p>
+      <button onClick={goToHomeScreen} style={{textAlign: 'center', marginTop: 'auto'}} className="HomeButton">Play Again</button>
+    </div>
+  );
+};
+
+export default GameScreen;
